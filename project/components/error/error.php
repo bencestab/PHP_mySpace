19d37b7cd4a58a665c1336178a1019844b7b034e
bobbyfromgeminus <info.geminus@gmail.com>
2023-09-23 17:33:33 +0200

    error page fixing

--- project/components/error/error.php
+++ project/components/error/error.php
@@ -1,11 +1,11 @@
     <section class="single h-100">
-        <div style="background-image: url('./assets/img/error.jpg');"></div>
+        <div style="background-image: url('/assets/img/error.jpg');"></div>
         <article>
             <h2>SAJNÁLATOS HIBA TÖRTÉNT</h2>
             Sajnáljuk, de a keresett hír vagy tartalom jelenleg nem elérhető.
             <br><br>
             Kérjük, ellenőrizze a weboldalunkat később, vagy keressen más érdekes témákat!
             <br><br>
-            <a href="index.php" class="link">< vissza a főoldalra</a>
+            <a href="/" class="link">< vissza a főoldalra</a>
         </article>
     </section>
